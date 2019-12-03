6fe3aa7a115fde15d2e052fd573732905b3dfcdd
Martin Daniel <mtin722@gmail.com>
2019-12-03 11:55:35 +1100

    Added link to twitter profile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => {
     <div>
       <h1>Contact Page</h1>
       <p>Hi, my name is Martin.</p>
-      <p>twitter: @martink</p>
+      <p>twitter: <a href="https://mobile.twitter.com/martink" target="_blank">@martink</a></p>
     </div>
   )
 }
