8821a8a68f2ad79c2f8caad62a3c596a0edc1f5f
Martin Daniel <mtin722@gmail.com>
2019-12-03 11:34:38 +1100

    Created about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Hi, my name is Martin</p>
+      <p>I am a full stack developer</p>
+    </div>
+  )
+}
+
+export default AboutPage
