69a61e2d900a99dc7f11edec1ab914b56790c013
Martin Daniel <mtin722@gmail.com>
2019-12-04 13:57:19 +1100

    Create blog pages

---
+++ src/components/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../layout'
+const Blog = () => {
+  return (
+    <Layout>
+      This is the blog template
+    </Layout>
+  )
+}
+
+export default Blog
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
     <Layout>
       <h1>Contact Page</h1>
       <p>Hi, my name is Martin.</p>
-      <p>twitter: <a href="https://mobile.twitter.com/martink" target="_blank">@martink</a></p>
+      {/* <p>twitter: <a href="https://mobile.twitter.com/martink" target="_blank">@martink</a></p> */}
     </Layout>
   )
 }
