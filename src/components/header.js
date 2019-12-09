ce02b6692fe95596db1d43b9d5d1055f8bc6fd35
Martin D K <mtin727@gmail.com>
2019-12-09 11:09:27 +1100

    Fixed header - now blog is active for subpages

--- src/components/header.js
+++ src/components/header.js
@@ -25,7 +25,7 @@ const Header = () => {
         <ul className={headerStyles.navList}>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
-          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} partiallyActive={true} to="/blog">Blog</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
         </ul>
       </nav>
