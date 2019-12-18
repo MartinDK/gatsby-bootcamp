0b7c42fc0ac670711a1b7036d31611922c45e76e
MartinDK <MartinDK@users.noreply.github.com>
2019-12-18 17:25:56 +1100

    Removed unused code

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,5 @@
 import React from "react"
-// import PropTypes from "prop-types"
 
-// Components
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -33,24 +31,6 @@ const TagsPage = ({
     </div>
   </Layout>
 )
-
-// TagsPage.propTypes = {
-//   data: PropTypes.shape({
-//     allMdx: PropTypes.shape({
-//       group: PropTypes.arrayOf(
-//         PropTypes.shape({
-//           fieldValue: PropTypes.string.isRequired,
-//           totalCount: PropTypes.number.isRequired,
-//         }).isRequired
-//       ),
-//     }),
-//     site: PropTypes.shape({
-//       siteMetadata: PropTypes.shape({
-//         title: PropTypes.string.isRequired,
-//       }),
-//     }),
-//   }),
-// }
 
 export default TagsPage
 
