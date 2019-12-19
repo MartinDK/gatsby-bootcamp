8b6bf1fbc6834380cf0e965f13560eb794678a66
Martin Daniel <mtin722@gmail.com>
2019-12-19 13:16:32 +1100

    Rename tag-list component

--- src/components/templates/tag-list.js
+++ src/components/templates/tag-list.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import tagListStyles from './tag-list.module.scss'
+
 const TagList = (props) => {
  
   let tags = ""
 
   if (props.tagsArray) {
     tags = props.tagsArray.map( tag => 
-      <span key={tag}><Link to={`/tags/${tag}`}>{tag}</Link> </span>
+      <span key={tag}><Link to={`/tags/${tag}`} className={tagListStyles.link}>{tag}</Link> </span>
     )
   }
   return (tags)
