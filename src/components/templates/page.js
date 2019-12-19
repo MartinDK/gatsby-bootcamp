edd0bda3666eb15924efaaf7cf5202129c53dee5
Martin Daniel <mtin722@gmail.com>
2019-12-19 13:17:39 +1100

    Updated page title container layout

--- src/components/templates/page.js
+++ src/components/templates/page.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 import Layout from '../layout'
 import Head from '../head'
+import TagsList from './tag-list';
 
 import pageStyles from './page.module.scss'
-import TagsList from './taglist'
 
 export const query = graphql`
   query ($slug: String!) {
@@ -28,8 +28,10 @@ const Page = (props) => {
       <Head />  
       <div className={pageStyles.pageTitle}>
         <h1>{props.data.mdx.frontmatter.title}</h1>
-        <TagsList tagsArray={props.data.mdx.frontmatter.tags} />
-        <p>{props.data.mdx.frontmatter.date}</p>
+        <div className={pageStyles.tagDateContainer}>
+          <TagsList tagsArray={props.data.mdx.frontmatter.tags} />
+          <span className={pageStyles.date}>{props.data.mdx.frontmatter.date}</span>
+        </div>
       </div>
       <div className={pageStyles.pageBody}>
         <MDXRenderer className={pageStyles.pageBody}>{props.data.mdx.body}</MDXRenderer>
