14f855ee60e0be52de42ff369b6cf8c10b6b032b
MartinDK <marty@live.com.au>
2022-05-16 14:31:11 +1000

    tag-list and blog style modules now import link mixin

--- src/components/templates/tag-list.js
+++ src/components/templates/tag-list.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import tagListStyles from './tag-list.module.scss'
+import tagListStyles from "./tag-list.module.scss"
 
 const TagList = (props) => {
- 
   let tags = ""
 
   if (props.tagsArray) {
-    tags = props.tagsArray.map( tag => 
-      <span key={tag}>
-        <Link to={`/tags/${tag}`} className={tagListStyles.link}>
+    tags = props.tagsArray.map((tag) => (
+      <span key={tag} className={tagListStyles.tags}>
+        <Link to={`/tags/${tag}`} className={tagListStyles}>
           {tag}
         </Link>
       </span>
-    )
+    ))
   }
-  return (tags)
+  return tags
 }
 
 export default TagList
